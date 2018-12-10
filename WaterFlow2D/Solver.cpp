7f4ffc0eaf54a12eb3347d19e61b358f3148ae92
Sam <sam.sutcliffe2@gmail.com>
2018-12-10 10:41:46 +0000

    Updated kernal function

--- WaterFlow2D/Solver.cpp
+++ WaterFlow2D/Solver.cpp
@@ -12,34 +12,53 @@ void Solver::UpdatePressure()
 	for(int i = 0;i < ParticleCount;++i)
 	{
 		auto & ParticleI = GetParticle(i); 
+		ParticleI.Density = 0;
 		for(int j = 0;j < ParticleCount;++j)
 		{
 			auto & ParticleJ = GetParticle(j); 
 			if(i != j)
 			{
-				Vector difference = ParticleI.Position - ParticleJ.Position; 
-				float DistanceSquared = difference.Dot(difference);
-
+				Vector difference = ParticleI.Position - ParticleJ.Position;
+				ParticleI.Density += ParticleJ.Mass; 
 			}
 		}
 	}
 }
-float Solver::SmoothingKernal(float Radius, float SmoothingParam)
+float Solver::SmoothingKernal(float Radius)
 {
 	float x = Radius/SmoothingParam;
+	const constexpr float factor = 1/(3.14*SmoothingParam*SmoothingParam*SmoothingParam);
 	if(x < 1)
 	{
-		return 1 - (1.5 * x * x) + (0.75 * x * x * x);
+		return factor * (1 - (1.5 * x * x) + (0.75 * x * x * x));
 	}
 	else if(x < 2)
 	{
-		return 0.25 * (2 - x) *  (2 - x) *  (2 - x); 
+		return factor * (0.25 * (2 - x) *  (2 - x) *  (2 - x)); 
 	}
 	else
 	{
 		return 0;
 	}
 }
+float Solver::SmoothingKernalGradient(float Radius)
+{
+	float x = Radius/SmoothingParam;
+	const constexpr float factor = 1/(3.14*SmoothingParam*SmoothingParam*SmoothingParam*SmoothingParam);
+	if(x < 1)
+	{
+		return factor * ((9/4 * x * x) + (3 * x));
+	}
+	else if(x < 2)
+	{
+		return factor * (-0.75 * (2 - x) *  (2 - x)); 
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 
 void Solver::UpdateConditions()
 {
@@ -49,7 +68,6 @@ void Solver::Intergrate()
 {
 	for(int i = 0;i < ParticleCount;++i)
 	{
-	//	auto Force =
 		auto & ParticleI = GetParticle(i); 
 		Vector Acceleration = (ParticleI.Force / ParticleI.Mass) * Solver::DeltaTime;
 		ParticleI.Position = (ParticleI.Position * 2) - ParticleI.PositionOld + Acceleration;
--- WaterFlow2D/Solver.h
+++ WaterFlow2D/Solver.h
@@ -4,13 +4,15 @@ class Solver{
 public:
 	static const constexpr int MaxParticles = 1000;
 	static const constexpr int DeltaTime = 1;
+	static const constexpr float SmoothingParam = 1;
 private:
 	std::array<Particle,MaxParticles> ParticleSwapList;
 	int ParticleCount;
 	void UpdateConditions();
 	void UpdatePressure();
 	void Intergrate();
-	float SmoothingKernal(float Radius,float SmoothingParam);
+	float SmoothingKernal(float Radius);
+	float SmoothingKernalGradient(float Radius);
 public:
 	Solver() = default;
 	~Solver() = default;
