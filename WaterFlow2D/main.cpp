96e38f80c3b5e097657a243ee82acc86167ead28
Sam <sam.sutcliffe2@gmail.com>
2019-01-03 14:21:24 +0000

    Finalised 2d testbed

--- WaterFlow2D/main.cpp
+++ WaterFlow2D/main.cpp
@@ -1,6 +1,7 @@
 #include "Solver.h"
 #include <iostream>
 #include <chrono>
+#include <array>
 void Update(Solver & solver)
 {
 	solver.Update();
@@ -8,21 +9,28 @@ void Update(Solver & solver)
 int main(int argc, char** args)
 {
 	std::ofstream out("data.txt");
-	constexpr const int RunTime = 1000;
+	std::ofstream timingout("timing.txt");
+	static constexpr const int RunTime = 1000;
 	constexpr const float WaterMass = 1;
 	std::ios_base::sync_with_stdio(false);
+	std::array<int,RunTime> TimingArray;
 	Solver solver;
 	solver.AddParticle(Vector(20,40), WaterMass);
-	//auto start = std::chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 	for(int t = 0;t < RunTime;++t)
 	{
 		if(t % 10 == 0){solver.AddParticle(Vector(20+(rand()%40)/15.0f,40),WaterMass);}
 		Update(solver);
 		solver.Print(t,out);
 		if (t % (RunTime >> 4) == 0) { std::cout << ((static_cast<float>(t) / RunTime) * 100.0) << "\n"; }
-		//auto end = std::chrono::high_resolution_clock::now();
+		auto end = std::chrono::high_resolution_clock::now();
+		TimingArray[t] = (end - start).count();
 		//std::cout<<(end - start).count()<<"\n";
-		//start = end;
+		start = end;
+	}
+	for (int t : TimingArray)
+	{
+		timingout << t << "\n";
 	}
 	return 0;
 }
